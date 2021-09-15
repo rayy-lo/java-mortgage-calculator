58a73c39bad6d36426a7c156e08cad9c4e6dca77
Raymond <raymond.lo@yahoo.com>
2021-09-15 17:16:19 -0400

    Formatted mortgage payment output

--- src/com/rayylo/mortgage_calculator/Main.java
+++ src/com/rayylo/mortgage_calculator/Main.java
@@ -1,9 +1,14 @@
 package com.rayylo.mortgage_calculator;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
         MortgageCalculator calc = new MortgageCalculator();
-        calc.getMortgageInput();
+        double unformattedPayment = calc.calculateMortage();
+        Locale locale = new Locale("en", "US");
+        System.out.println(NumberFormat.getCurrencyInstance(locale).format(unformattedPayment));
     }
 }
