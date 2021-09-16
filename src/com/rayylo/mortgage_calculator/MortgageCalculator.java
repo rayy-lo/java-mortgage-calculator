3bfec49664c293532968334e3eea6403f38c1fbf
Raymond <raymond.lo@yahoo.com>
2021-09-16 00:44:36 -0400

    Added error handling

--- src/com/rayylo/mortgage_calculator/MortgageCalculator.java
+++ src/com/rayylo/mortgage_calculator/MortgageCalculator.java
@@ -11,18 +11,38 @@ public class MortgageCalculator {
 
     public void getMortgageInput(){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter Principal Amount: ");
-        principal = scanner.nextInt();
-        System.out.print("Annual Interest Rate (%): ");
-        annualInterest = scanner.nextFloat();
-        System.out.print("Period (Years): ");
-        amortizationPeriod = scanner.nextByte();
+
+        while(true){
+            System.out.print("Principal ($1K - $1M):");
+            principal = scanner.nextInt();
+            if(principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a number between 1,000 and 1,000,000 ");
+        }
+
+        while(true){
+            System.out.print("Annual Interest Rate (%): ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest > 0 && annualInterest < 30)
+                break;
+            System.out.println("Enter a value greater than 0 and less than 30");
+        }
+
+        while(true){
+            System.out.print("Period (Years): ");
+            amortizationPeriod = scanner.nextByte();
+            if(amortizationPeriod >= 1 && amortizationPeriod <= 30)
+                break;
+            System.out.println("Enter a value between 1 and 30");
+        }
+
     }
 
     public double calculateMortage(){
-        getMortgageInput();
-        double monthlyInterest = (annualInterest / 100) / 12;
-        int totalPayments = amortizationPeriod * 12;
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+        double monthlyInterest = (annualInterest / PERCENT) / MONTHS_IN_YEAR;
+        int totalPayments = amortizationPeriod * PERCENT;
         return principal * (monthlyInterest * Math.pow((1 + monthlyInterest), totalPayments)) / (Math.pow((1+monthlyInterest), totalPayments) - 1);
     }
 }
